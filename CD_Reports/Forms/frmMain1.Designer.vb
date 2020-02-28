7a99fe56c8da4a52f8715f016740a6a6ae5189d2
Eric Hirsch <ericonsi@yahoo.com>
2020-02-28 17:03:47 -0500

    x

--- CD_Reports/Forms/frmMain1.Designer.vb
+++ CD_Reports/Forms/frmMain1.Designer.vb
@@ -28,51 +28,57 @@ Partial Class frmMain1
         Me.chkOtherReminderInfo = New System.Windows.Forms.CheckBox()
         Me.btnGo = New System.Windows.Forms.Button()
         Me.pnlReportComponents = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'chk3MonthDue
         '
         Me.chk3MonthDue.AutoSize = True
-        Me.chk3MonthDue.Location = New System.Drawing.Point(66, 70)
+        Me.chk3MonthDue.BackColor = System.Drawing.Color.LightBlue
+        Me.chk3MonthDue.Location = New System.Drawing.Point(66, 82)
         Me.chk3MonthDue.Name = "chk3MonthDue"
         Me.chk3MonthDue.Size = New System.Drawing.Size(147, 17)
         Me.chk3MonthDue.TabIndex = 0
         Me.chk3MonthDue.Text = "Three Month Screen Due"
-        Me.chk3MonthDue.UseVisualStyleBackColor = True
+        Me.chk3MonthDue.UseVisualStyleBackColor = False
         '
         'chk6MonthDue
         '
         Me.chk6MonthDue.AutoSize = True
-        Me.chk6MonthDue.Location = New System.Drawing.Point(66, 99)
+        Me.chk6MonthDue.BackColor = System.Drawing.Color.LightBlue
+        Me.chk6MonthDue.Location = New System.Drawing.Point(66, 111)
         Me.chk6MonthDue.Name = "chk6MonthDue"
         Me.chk6MonthDue.Size = New System.Drawing.Size(133, 17)
         Me.chk6MonthDue.TabIndex = 1
         Me.chk6MonthDue.Text = "Six Month Screen Due"
-        Me.chk6MonthDue.UseVisualStyleBackColor = True
+        Me.chk6MonthDue.UseVisualStyleBackColor = False
         '
         'chkMDDue
         '
         Me.chkMDDue.AutoSize = True
-        Me.chkMDDue.Location = New System.Drawing.Point(66, 128)
+        Me.chkMDDue.BackColor = System.Drawing.Color.LightBlue
+        Me.chkMDDue.Location = New System.Drawing.Point(66, 140)
         Me.chkMDDue.Name = "chkMDDue"
         Me.chkMDDue.Size = New System.Drawing.Size(91, 17)
         Me.chkMDDue.TabIndex = 3
         Me.chkMDDue.Text = "MD Appt Due"
-        Me.chkMDDue.UseVisualStyleBackColor = True
+        Me.chkMDDue.UseVisualStyleBackColor = False
         '
         'chkOtherReminderInfo
         '
         Me.chkOtherReminderInfo.AutoSize = True
-        Me.chkOtherReminderInfo.Location = New System.Drawing.Point(66, 157)
+        Me.chkOtherReminderInfo.BackColor = System.Drawing.Color.LightBlue
+        Me.chkOtherReminderInfo.Location = New System.Drawing.Point(66, 169)
         Me.chkOtherReminderInfo.Name = "chkOtherReminderInfo"
         Me.chkOtherReminderInfo.Size = New System.Drawing.Size(121, 17)
         Me.chkOtherReminderInfo.TabIndex = 5
         Me.chkOtherReminderInfo.Text = "Other Reminder Info"
-        Me.chkOtherReminderInfo.UseVisualStyleBackColor = True
+        Me.chkOtherReminderInfo.UseVisualStyleBackColor = False
         '
         'btnGo
         '
-        Me.btnGo.Location = New System.Drawing.Point(94, 210)
+        Me.btnGo.Location = New System.Drawing.Point(94, 222)
         Me.btnGo.Name = "btnGo"
         Me.btnGo.Size = New System.Drawing.Size(75, 23)
         Me.btnGo.TabIndex = 6
@@ -81,16 +87,40 @@ Partial Class frmMain1
         '
         'pnlReportComponents
         '
-        Me.pnlReportComponents.Location = New System.Drawing.Point(40, 51)
+        Me.pnlReportComponents.BackColor = System.Drawing.Color.LightBlue
+        Me.pnlReportComponents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlReportComponents.Location = New System.Drawing.Point(40, 71)
         Me.pnlReportComponents.Name = "pnlReportComponents"
-        Me.pnlReportComponents.Size = New System.Drawing.Size(184, 141)
+        Me.pnlReportComponents.Size = New System.Drawing.Size(184, 130)
         Me.pnlReportComponents.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(30, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(211, 16)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "HealthConnect Client Call List"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(196, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Choose one or more report components:"
         '
         'frmMain1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(270, 285)
+        Me.BackColor = System.Drawing.Color.AliceBlue
+        Me.ClientSize = New System.Drawing.Size(271, 273)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnGo)
         Me.Controls.Add(Me.chkOtherReminderInfo)
         Me.Controls.Add(Me.chkMDDue)
@@ -110,4 +140,6 @@ Partial Class frmMain1
     Friend WithEvents chkOtherReminderInfo As CheckBox
     Friend WithEvents btnGo As Button
     Friend WithEvents pnlReportComponents As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
---
+++ CD_Reports_MiddleTier/ReportComponents/ReportComponent_RemindersList.vb
@@ -0,0 +1,32 @@
+﻿Imports CD_Reports_MiddleTier.AppSetter
+Public Class ReportComponent_RemindersList
+    Implements IReportComponent
+
+    Function GetOrdinal() As Integer Implements IReportComponent.GetOrdinal
+        Return 1
+    End Function
+    Function GetDescription() As String Implements IReportComponent.GetDescription
+        Return "Reminders List items"
+    End Function
+    Function GetSelectQuery() As EH_DataUtilities.EH_QueryBuilder Implements IReportComponent.GetSelectQuery
+
+        Dim ehq As New EH_DataUtilities.EH_QueryBuilder(GetHostSqlServer())
+
+        ehq.ADD_TO_SELECT("ClientID, Item, Details ")
+        ehq.ASSIGN_FROM_STATEMENT("tblReminder")
+        ehq.ADD_TO_WHERE("Program ='HealthConnect' AND Reminder_date >= getdate()")
+
+        Return ehq
+
+    End Function
+    Function GetUpdateQuery(CID As Integer) As EH_DataUtilities.EH_QueryBuilder Implements IReportComponent.GetUpdateQuery
+
+        Dim ehq As New EH_DataUtilities.EH_QueryBuilder(GetHostSqlServer())
+
+        ehq.ASSIGN_FIRST_LINE("UPDATE tblMasterClientCallList_HC SET Six_Month_Screen_Due = 'TRUE'")
+        ehq.ADD_TO_WHERE("ClientID =" & CID)
+        Return ehq
+
+    End Function
+
+End Class
